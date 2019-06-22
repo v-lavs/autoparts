f0bba9ecec2326eb78d04c4a3d78cc18e4d88ca5
v-lavs <lavskavanda@gmail.com>
2019-06-22 23:03:36 +0300

    add sections to home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 /**
  *To include js file from libraries write: `//= include ./path-to-file`
-* */
+ * */
 
 //= include ../lib/jquery-3.3.1.min.js
 //= include ../lib/owl-slider/js/owl.carousel.js
@@ -10,83 +10,84 @@
  **/
 
 $(document).ready(function () {
-  $('a[href="#"]').on('click', function (e) {
-    e.preventDefault();
-  });
-  /**
-   * STICKY-HEADER
-   **/
+    $('a[href="#"]').on('click', function (e) {
+        e.preventDefault();
+    });
+    /**
+     * STICKY-HEADER
+     **/
 
-  var scrolled;
+    var scrolled;
 
-  function addScrollClass () {
-    scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrolled > 40) {
-      $('.header').addClass('scroll');
-    } else {
-      $('.header').removeClass('scroll');
+    function addScrollClass() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 40) {
+            $('.header').addClass('scroll');
+        } else {
+            $('.header').removeClass('scroll');
+        }
     }
-  }
 
-  addScrollClass();
-
-  $(window).on('scroll', function () {
     addScrollClass();
-  });
 
-  /**
-   *  TOGGLE ACTIVE CLASS
-   * */
+    $(window).on('scroll', function () {
+        addScrollClass();
+    });
 
-  var nav = $('.header-menu');
+    /**
+     *  TOGGLE ACTIVE CLASS
+     * */
 
-  $('.header .nav-link').click(function (e) {
-    e.preventDefault();
-    $('.header .nav-link').removeClass('active');
-    $(this).addClass('active');
+    var nav = $('.header-menu');
 
-    nav.removeClass('open');
-    jQuery('.backdrop').fadeOut();
-  });
+    $('.header .nav-link').click(function (e) {
+        e.preventDefault();
+        $('.header .nav-link').removeClass('active');
+        $(this).addClass('active');
 
-  /**
-   * MOB MENU SCRIPT
-   **/
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
 
-  $('.burger').click(function (e) {
-    e.preventDefault();
-    nav.addClass('open');
-    jQuery('.backdrop').fadeIn();
-  });
+    /**
+     * MOB MENU SCRIPT
+     **/
 
-  $('.close-nav, .backdrop').click(function (e) {
-    e.preventDefault();
-    nav.removeClass('open');
-    jQuery('.backdrop').fadeOut();
-  });
+    $('.burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
 
-  jQuery(".owl-carousel").owlCarousel({
-    loop: !0,
-    touchDrag: !0,
-    smartSpeed: 1e3,
-    margin: 30,
-    dots: !1,
-    responsiveClass: !0,
-    nav: !0,
-    navContainer: !1,
-    // navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      970: {
-        items: 3,
-        loop: !0
-      }
-    }
-  });
+    $('.close-nav, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+    jQuery(".owl-carousel").owlCarousel({
+        loop: false,
+        touchDrag: true,
+        smartSpeed: 500,
+        margin: 30,
+        dots: false,
+        responsiveClass: true,
+        nav: true,
+        navContainer: ".navigation",
+        // navText: ["&lang;", "&rang;"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            970: {
+                items: 3,
+                loop: false,
+                rewind: true
+            }
+        }
+    });
 
 });
